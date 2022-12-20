a5251dcd3bf21d25df676994ebf69ad7dc45fa00
Danilo Ramalho <daniloti@hotmail.com>
2022-12-19 21:22:44 -0300

    Componente de alertas

--- score_poker/src/components/Alert.jsx
+++ score_poker/src/components/Alert.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import Alert from 'react-bootstrap/Alert';
-import { useNavigate } from 'react-router-dom';
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
 
-function AlertM({ message }) {
+function AlertM({
+  message, func, type, isTrue,
+}) {
+  return (
+    <ToastContainer position="middle-center">
+      <Toast variant={type.toLowerCase()} bg={type.toLowerCase()} onClose={func} show={isTrue}>
+        <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+          <strong className="me-auto">{type}</strong>
+          {/* <small className="text-muted"></small> */}
+        </Toast.Header>
+        <Toast.Body>{message}</Toast.Body>
+      </Toast>
+    </ToastContainer>
+  );
+}
+
+/* function AlertM() {
   const navigate = useNavigate();
 
   return (
@@ -11,10 +28,13 @@ function AlertM({ message }) {
       <Alert.Heading>{message}</Alert.Heading>
     </Alert>
   );
-}
+} */
 
 AlertM.propTypes = {
   message: propTypes.string.isRequired,
+  func: propTypes.func.isRequired,
+  type: propTypes.string.isRequired,
+  isTrue: propTypes.bool.isRequired,
 };
 
 export default AlertM;
