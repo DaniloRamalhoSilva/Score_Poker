89a0a1d083de493a1759140c3a58d244ebe1b1da
Danilo Ramalho <daniloti@hotmail.com>
2022-12-19 21:24:31 -0300

    Componente de podio

---
+++ score_poker/src/components/ImagePodio.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import { Container } from 'react-bootstrap';
+import imgPdodio from '../../public/podio.png';
+
+const AVATAR = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgUNaoFwOOa3sOnMoc8CVUJ65bhS822etxVQ&usqp=CAU';
+
+function ImagePodio({ rank }) {
+  return (
+    rank.length > 3 && (
+      <Container className="mt-4">
+        <h1>Ranking</h1>
+        <div className="containerImage">
+          <img className="imgPodio" src={imgPdodio} alt="" />
+          <img className="i i1" src={(rank[0].image) ? rank[0].image : AVATAR} alt="foto" />
+          <img className="i i2" src={(rank[1].image) ? rank[1].image : AVATAR} alt="foto" />
+          <img className="i i3" src={(rank[2].image) ? rank[2].image : AVATAR} alt="foto" />
+        </div>
+      </Container>
+    )
+  );
+}
+ImagePodio.propTypes = {
+  rank: propTypes.arrayOf(
+    propTypes.shape().isRequired,
+  ).isRequired,
+};
+
+export default ImagePodio;
