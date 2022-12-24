1f143b6196085ed5e58587c3807d376c93c8c0e4
Danilo Ramalho <daniloti@hotmail.com>
2022-12-24 16:34:26 -0300

    Deploy

--- score_poker/src/services/varUteis.js
+++ score_poker/src/services/varUteis.js
@@ -1,5 +1,5 @@
 const AVATAR = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgUNaoFwOOa3sOnMoc8CVUJ65bhS822etxVQ&usqp=CAU';
-const baseURL = { baseURL: 'http://localhost:5180' };
+const baseURL = { baseURL: 'https://scorepokerapi-production-fc9d.up.railway.app/' };
 
 export {
   AVATAR,
